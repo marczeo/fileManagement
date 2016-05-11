91ba48fcd8d3033dc58dd3694027956b441384c5
marczeo <marco.gomez@outlook.com>
2016-05-10 21:31:36 -0500

    Instalación
    Modificación a rutas relativas por el servidor local.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fileManagementApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fileManagementApp
+ */
+angular.module('fileManagementApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
